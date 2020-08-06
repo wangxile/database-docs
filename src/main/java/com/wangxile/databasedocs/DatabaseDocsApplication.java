1792d27eba1a429a5abe77bafe382098cd339a0b
wangqi <12345678>
2020-08-06 13:56:46 +0800

    初始化

---
+++ src/main/java/com/wangxile/databasedocs/DatabaseDocsApplication.java
@@ -0,0 +1,13 @@
+package com.wangxile.databasedocs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatabaseDocsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DatabaseDocsApplication.class, args);
+    }
+
+}
